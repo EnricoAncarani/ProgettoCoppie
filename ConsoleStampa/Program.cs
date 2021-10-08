2f398317c531c291788b5e52e1b3b97ca8d16d1e
Enrico Ancarani <ENRICO.ANCARANI@local.ittcesena.it>
2021-10-08 09:33:41 +0200

    nafbnasdgf

--- ConsoleStampa/Program.cs
+++ ConsoleStampa/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleStampa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ciao belloooo!");
+            Console.WriteLine("Ciao !");
             Console.ReadKey();
         }
     }
